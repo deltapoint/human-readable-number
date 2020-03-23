a85c8ed8393295059c67297cf218a2694831e9ed
deltapoint <60572499+deltapoint@users.noreply.github.com>
2020-03-23 17:26:53 +0300

    feat: implement 'human-readable-number' task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (n) {
+    if (n < 1) return 'zero';
+    if (n < 20) return ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'][n - 1];
+    if (n < 100) return ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'][Math.floor(n / 10) - 2] + (n % 10 ? ' ' + toReadable(n % 10) : '');
+    if (n < 1000) return toReadable(Math.floor(n / 100)) + ' hundred' + (n % 100 ? ' ' + toReadable(n % 100) : '');
 }
